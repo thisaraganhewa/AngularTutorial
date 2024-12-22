6b9238048c578f721eae565350ab7b0f27a5b9d3
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-12-22 18:17:00 +0530

    routing 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+
+        return import('./Pages/home/home.component').then(m => m.HomeComponent)
+
+    }
+
+},
+{
+
+    path: 'todos',
+    loadComponent: () =>{
+
+        return import('./Pages/todos/todos.component').then(m => m.TodosComponent)
+
+    }
+
+}
+];
